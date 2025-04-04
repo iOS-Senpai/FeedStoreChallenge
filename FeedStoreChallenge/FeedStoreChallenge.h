fbd864317624437b80321d7f094f74fbceac65f2
Pradeep Kumar <mukku.kumar@telusagcg.com>
2025-04-04 14:36:56 +0530

    Initial Commit

---
+++ FeedStoreChallenge/FeedStoreChallenge.h
@@ -0,0 +1,18 @@
+//
+//  FeedStoreChallenge.h
+//  FeedStoreChallenge
+//
+//  Created by xavient on 04/04/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FeedStoreChallenge.
+FOUNDATION_EXPORT double FeedStoreChallengeVersionNumber;
+
+//! Project version string for FeedStoreChallenge.
+FOUNDATION_EXPORT const unsigned char FeedStoreChallengeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeedStoreChallenge/PublicHeader.h>
+
+
